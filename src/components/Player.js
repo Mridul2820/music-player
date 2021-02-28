2e18c50a63e632f9f40432ad1415efb157129cb0
Mridul2820 <immridulpanda2820@gmail.com>
2021-02-28 17:40:49 +0530

    icons import and added to dom

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,16 +1,19 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 const Player = () => {
     return (
         <div className='player'>
-            <div className="time-control">
+            <div className="time-control"> 
                 <p>start time</p>
                 <input type="range"/>
                 <p>end time</p>
             </div>
             <div className="play-control">
-                
+                <FontAwesomeIcon className='skip-back' size="2x" icon={faAngleLeft} />
+                <FontAwesomeIcon className='play' size="2x" icon={faPlay} />
+                <FontAwesomeIcon className='skip-forward' size="2x" icon={faAngleRight} />
             </div>
         </div>
     )
