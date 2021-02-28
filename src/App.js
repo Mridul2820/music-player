d369f431821bbe4e19ec7a1e494cb5ea53d9a368
Mridul2820 <immridulpanda2820@gmail.com>
2021-02-28 19:35:31 +0530

    use ref to grab the current audio

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     return (
         <div>
             <Song currentSong={currentSong} />
-            <Player />
+            <Player currentSong={currentSong} />
         </div>
     )
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import { useRef } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({ currentSong }) => {
+    const audioRef = useRef(null)
+
+    const playSongHandler = () => {
+        console.log(audioRef.current);
+    }
+
     return (
         <div className='player'>
             <div className="time-control"> 
@@ -12,9 +18,10 @@ const Player = () => {
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className='skip-back' size="2x" icon={faAngleLeft} />
-                <FontAwesomeIcon className='play' size="2x" icon={faPlay} />
+                <FontAwesomeIcon className='play' size="2x" icon={faPlay} onClick={playSongHandler} />
                 <FontAwesomeIcon className='skip-forward' size="2x" icon={faAngleRight} />
             </div>
+            <audio ref={audioRef} src={currentSong.audio}></audio>
         </div>
     )
 }
