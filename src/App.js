514bdf6e3127439d157b0dc00a6825636680ed6f
Mridul2820 <immridulpanda2820@gmail.com>
2021-02-28 19:14:46 +0530

    add the current song to dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 
 import Player from './components/Player'
 import Song from './components/Song'
@@ -8,9 +8,12 @@ import data from './utils'
 import './styles/App.scss'
 
 const App = () => {
+    const [songs, setSongs] = useState(data())
+    const [currentSong, setCurrentSong] = useState(songs[0])
+
     return (
         <div>
-            <Song />
+            <Song currentSong={currentSong} />
             <Player />
         </div>
     )
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Song = () => {
+const Song = ({ currentSong }) => {
     return (
         <div className='song-container'>
-            <h1>picture</h1>
-            <h1>song name</h1>
-            <h1>artist</h1>
+            <img src={currentSong.cover} alt={currentSong.name} />
+            <h2>{currentSong.name}</h2>
+            <h3>{currentSong.artist}</h3>
         </div>
     )
 }
